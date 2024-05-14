f00f71377dc4901270fa5185ffc01451f5b7e40d
Gustav Brodin <brodin.gustav@gmail.com>
2024-05-14 13:12:45 +0200

    Commit 5 - Skapat interface för Courses. Ej importerat än

---
+++ src/models/courses.ts
@@ -0,0 +1,13 @@
+export interface Courses{
+    courseCode: string;
+    subjectCode: string;
+    level: string;
+    progression: string;
+    courseName: string;
+    points: number;
+    institutionCode: string;
+    subject: string;
+    syllabus: string;
+}
+
+//Interface som struktur för array av JSON-fil. Ska importeras i courses.component.ts?
